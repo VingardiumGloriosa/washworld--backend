f0512d7c7fccde1de56f15a780ee5998efdc6427
rudasrudas <zdanaviciusjustas1@gmail.com>
2024-05-26 10:51:10 +0200

    fixed user?

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Location } from './entities/location.entity';
 import { LocationService } from './location.service';
 import { LocationController } from './location.controller';
+import { UserService } from '@src/user/user.service';
+import { UserModule } from '@src/user/user.module';
 
 @Module({
   controllers: [LocationController],
