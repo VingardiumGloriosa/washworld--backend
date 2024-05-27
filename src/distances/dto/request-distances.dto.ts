18b58ffa6205516abf1a638c9410fbf586624def
rudasrudas <zdanaviciusjustas1@gmail.com>
2024-05-27 14:02:25 +0200

    request distances update

--- src/distances/dto/request-distances.dto.ts
+++ src/distances/dto/request-distances.dto.ts
@@ -1,8 +1,9 @@
-import { IsArray, IsNumber, ValidateNested } from 'class-validator';
+import { IsArray, IsNumber, IsOptional, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class LocationDto {
     @IsNumber()
+    @IsOptional()
     readonly id: number; 
 
     @IsNumber()
@@ -16,9 +17,4 @@ export class CalculateDistancesDto {
     @ValidateNested()
     @Type(() => LocationDto)
     currentLocation: LocationDto;
-
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => LocationDto)
-    destinationLocations: LocationDto[];
 }
